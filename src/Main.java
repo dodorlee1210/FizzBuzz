7791d449c404ca98899ea3c732757cdd191ad4d8
Dorothy Lee <dot.lee@mail.utoronto.ca>
2023-09-11 17:58:09 -0400

    Retry of Task 3.1~2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+//        FizzBuzz.doFizzBuzz(100);
+        // calls the method that takes in one integer parameter (cz it changed after extracting)
+        FizzBuzz.main(); // runs after removing String[] args from main in FizzBuzz.java
+    }
 }
