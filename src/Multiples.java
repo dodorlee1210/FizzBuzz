d5ec4cc16baf105e84289a2bf5aa6d7dd0129228
Dorothy Lee <dot.lee@mail.utoronto.ca>
2023-09-11 17:28:57 -0400

    Code written for counting multiples of 3 & 5 less than 1000

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Multiples {
+import javax.swing.text.StyledEditorKit;
+
+public class Multiples {
+    public static void main(String[] args) {
+
+        int count = 0;
+
+        for (int i = 1; i < 1000; i++) {
+
+            Boolean multiplesOf3 = i % 3 == 0;
+            Boolean multiplesOf5 = i % 5 == 0;
+
+            if (multiplesOf5 || multiplesOf3) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
 }
